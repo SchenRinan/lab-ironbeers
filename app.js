2257b1e8b89f10c604c92ebc55f16205910e6453
Rinan <jandrewschnell@gmail.com>
2023-05-20 16:24:20 +0200

    day 0

--- app.js
+++ app.js
@@ -13,13 +13,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
 // ...
+
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/beers', (req, res) => {
+  res.render('beers');
+});
+app.get('/random-beer', (req, res) => {
+  res.render('randombeer');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
